4bd6a7f4f964eeae68a30de40455509b06c1b41d
Hélias <helias.gamonet@laposte.net>
2024-11-27 00:07:50 +0100

    Instances data asset

---
+++ Source/L3_Project/Public/Instances/InstanceDataAsset.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InstanceDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L3_PROJECT_API UInstanceDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString InstanceName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString MapPath = "/Game/_Project/000-Game/Maps/InstanceMap";
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* InstanceImage;
+};
