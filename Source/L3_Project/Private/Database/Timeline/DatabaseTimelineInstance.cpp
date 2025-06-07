764dc32f09e40adf53e29a92028b7e6fcec7a57f
Guillaume <Guillaume.wiart@outlook.fr>
2025-06-07 14:20:19 +0200

    Add timeline instance

---
+++ Source/L3_Project/Private/Database/Timeline/DatabaseTimelineInstance.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Database/Timeline/DatabaseTimelineInstance.h"
+
+DatabaseTimelineInstance::DatabaseTimelineInstance()
+{
+}
+
+DatabaseTimelineInstance::~DatabaseTimelineInstance()
+{
+}
