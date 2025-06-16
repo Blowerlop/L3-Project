6bfd1e6c630c085e417fc6242160dde12d2935ec
Blowerlop <Blowerlop@users.noreply.github.com>
2025-06-16 04:13:29 +0200

    Add new BTDecorator Cooldown with multiplicator

---
+++ Source/L3_Project/Public/AI/Decorators/BTDecorator_CustomCooldown.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_CustomCooldown.generated.h"
+
+/**
+ * 
+ */
+
+struct FBTCooldownDecoratorMemory
+{
+	double LastUseTimestamp;
+	uint8 bRequestedRestart : 1;
+};
+
+UCLASS(HideCategories=(Condition))
+class L3_PROJECT_API UBTDecorator_CustomCooldown : public UBTDecorator
+{
+	GENERATED_UCLASS_BODY()
+
+	/** max allowed time for execution of underlying node */
+	UPROPERTY(Category=Decorator, EditAnywhere)
+	float CoolDownTime;
+	UPROPERTY(Category=Decorator, EditAnywhere)
+	FBlackboardKeySelector MultiplicatorBlackboardKey;
+
+	//~ Begin UObject Interface
+	virtual void PostLoad() override;
+	//~ End UObject Interface
+
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+	virtual void InitializeMemory(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, EBTMemoryInit::Type InitType) const override;
+	virtual void CleanupMemory(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, EBTMemoryClear::Type CleanupType) const override;
+	virtual uint16 GetInstanceMemorySize() const override;
+	virtual void DescribeRuntimeValues(const UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, EBTDescriptionVerbosity::Type Verbosity, TArray<FString>& Values) const override;
+	virtual FString GetStaticDescription() const override;
+	virtual float GetCoolDownTimeWithMultiplicator(const UBehaviorTreeComponent& OwnerComp) const;
+
+#if WITH_EDITOR
+	virtual FName GetNodeIconName() const override;
+#endif // WITH_EDITOR
+
+protected:
+
+	virtual void OnNodeDeactivation(FBehaviorTreeSearchData& SearchData, EBTNodeResult::Type NodeResult) override;
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
